2fcc1beeca4d89849a45d43e9e3940b32d8ee197
Leo <leo@fuchus.de>
2018-10-28 18:53:17 +0100

    Fixed setup

--- setup.py
+++ setup.py
@@ -1,11 +1,16 @@
 from setuptools import setup
+from setuptools.command.install import install as _install
 
+class Install(_install):
+        def run(self):
+         _install.do_egg_install(self)
+         import nltk
+         nltk.download("popular")
 setup(
    name='Word replacer',
    version='1.0',
    description='A module that replaces certain words',
    author='Leonardo Martinho',
    author_email='leo@fuchus.de',
-   packages=['word_replacer, API'],
-   install_requires=['requests', 'nltk', 'argparse', 're', 'sys', 'pathlib2']
+   install_requires=['requests', 'nltk', 'argparse', 'pathlib2']
 )
